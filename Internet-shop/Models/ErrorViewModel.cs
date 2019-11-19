dfd7907ec684ecf60171ecbb101bb7734886c3c4
iva_bogdan <iva_bogdan@ukr.net>
2019-11-19 21:57:17 +0200

    Добавьте файлы проекта.

---
+++ Internet-shop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Internet_shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
