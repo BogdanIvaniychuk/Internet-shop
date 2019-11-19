c210df9fd54d4b75fe3a16a6481108033374edca
mishapomorskyi@gmail.com <mishapomorskyi@gmail.com>
2019-11-19 22:54:34 +0200

    new

--- Internet-shop/Controllers/HomeController.cs
+++ Internet-shop/Controllers/HomeController.cs
@@ -15,6 +15,10 @@ namespace Internet_shop.Controllers
             return View();
         }
 
+        public IActionResult NewInd()
+        {
+            return View();
+        }
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
