73dc15206ab55b5e1925e4b12d93c79a61438b79
Misha Pomorskyi <mishapomorskyi@gmail.com>
2019-11-20 19:48:46 +0200

    Added ShirtContext and ConnectionString in appsettings.json

---
+++ Internet-shop/Models/ShirtContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Internet_shop.Models;
+
+namespace EFDataApp.Models
+{
+    public class ShirtContext : DbContext
+    {
+        public DbSet<Shirt> Shirts { get; set; }
+        public ShirtContext(DbContextOptions<ShirtContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
